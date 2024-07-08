58feb0a7afd94a8bb7297980c87ed57f65176202
booploops <49113086+booploops@users.noreply.github.com>
2024-07-08 01:11:57 -0700

    changed events

--- src/api/ciderapi-types/PAPIEvents.ts
+++ src/api/ciderapi-types/PAPIEvents.ts
@@ -1,8 +1,8 @@
 export type PAPIEvents =
     'app:ready' |
     'shell:layout_type_changed' |
-    'shell:immersive_opened' |
-    'shell:immersive_closed' |
-    'shell:miniplayer_opened' |
-    'shell:miniplayer_closed' |
-    'browser:page_changed' 
+    'immersive:opened' |
+    'immersive:closed' |
+    'miniplayer:opened' |
+    'miniplayer:closed' |
+    'browser:page_changed'
