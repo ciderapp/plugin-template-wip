4567f3bd32f64033a35cbcb21a2c0e8345c13090
booploops <49113086+booploops@users.noreply.github.com>
2024-07-09 21:03:29 -0700

    allows for cider components to be used

--- src/pages/CustomPage.vue
+++ src/pages/CustomPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {computed} from 'vue';
 import HelloWorld from '../components/HelloWorld.vue';
+import CComponent from '../api/CComponent.vue';
 
 const cfg = computed(() => {
     return window.CiderApp.config.getRef();
@@ -12,6 +13,8 @@ const cfg = computed(() => {
     <h1 class="apple-heading">
         My Custom Page
     </h1>
+
+    <CComponent name="MojavePlayer"></CComponent>
 
     {{ cfg.general.displayName }}
 
